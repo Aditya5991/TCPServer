8846e28705bd18970e8d86344d4db346ca86cae3
Aditya Bhende <bhendeaditya@gmail.com>
2022-12-30 13:51:52 +0530

    Added TCP code to namespace net::tcp.

--- TCPServer/Common.h
+++ TCPServer/Common.h
@@ -2,5 +2,9 @@
 
 #define CRLF "\r\n"
 
-#define BEGIN_NAMESPACE_TCP namespace TCP {
-#define END_NAMESPACE_TCP   }
+#define BEGIN_NAMESPACE_NET namespace net {
+#define END_NAMESPACE_NET   }
+
+
+#define BEGIN_NAMESPACE_TCP     BEGIN_NAMESPACE_NET namespace tcp {
+#define END_NAMESPACE_TCP       END_NAMESPACE_NET   }
